06888349dc5f01fce3f49f5edb1490e3ff6f12a9
Ben Sully <ben.sully88@gmail.com>
2024-07-19 13:13:54 +0100

    feat: add augurs-clustering crate with DBSCAN algorithm

---
+++ crates/augurs-clustering/benches/dbscan.rs
@@ -0,0 +1,24 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use augurs_clustering::Dbscan;
+use augurs_dtw::DistanceMatrix;
+
+fn dbscan(c: &mut Criterion) {
+    let distance_matrix = include_str!("../data/dist.csv")
+        .lines()
+        .map(|l| {
+            l.split(',')
+                .map(|s| s.parse::<f64>().unwrap())
+                .collect::<Vec<f64>>()
+        })
+        .collect::<Vec<Vec<f64>>>();
+    let distance_matrix = DistanceMatrix::try_from_square(distance_matrix).unwrap();
+    c.bench_function("dbscan", |b| {
+        b.iter(|| {
+            Dbscan::new(10.0, 3).fit(&distance_matrix);
+        });
+    });
+}
+
+criterion_group!(benches, dbscan);
+criterion_main!(benches);
