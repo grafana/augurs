937c102c76db482c38e0bf4192c56fe0a11ec312
Ben Sully <ben.sully@grafana.com>
2024-11-16 00:11:10 +0000

    fix: actually use wasmstan in wasmstan example (#169)

--- examples/forecasting/examples/prophet_wasmstan.rs
+++ examples/forecasting/examples/prophet_wasmstan.rs
@@ -1,17 +1,7 @@
-//! Example of using the Prophet model with the cmdstan optimizer.
-//!
-//! To run this example, you must first download the Prophet Stan model
-//! and libtbb shared library into the `prophet_stan_model` directory.
-//! The easiest way to do this is to run the `download-stan-model`
-//! binary in the `augurs-prophet` crate:
-//!
-//! ```sh
-//! $ cargo run --features download --bin download-stan-model
-//! $ cargo run --example prophet_cmdstan
-//! ```
-use std::{collections::HashMap, time::Duration};
+//! Example of using the Prophet model with the wasmstan optimizer.
+use std::collections::HashMap;
 
-use augurs::prophet::{cmdstan::CmdstanOptimizer, Prophet, Regressor, TrainingData};
+use augurs::prophet::{wasmstan::WasmstanOptimizer, Prophet, Regressor, TrainingData};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
@@ -41,13 +31,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ]))
         .unwrap();
 
-    let cmdstan = CmdstanOptimizer::with_prophet_path("prophet_stan_model/prophet_model.bin")?
-        .with_poll_interval(Duration::from_millis(100))
-        .with_refresh(50);
-    // If you were using the embedded version of the cmdstan model, you'd use this:
-    // let cmdstan = CmdstanOptimizer::new_embedded();
-
-    let mut prophet = Prophet::new(Default::default(), cmdstan);
+    let wasmstan = WasmstanOptimizer::new();
+    let mut prophet = Prophet::new(Default::default(), wasmstan);
     prophet.add_regressor("foo".to_string(), Regressor::additive());
     prophet.add_regressor("bar".to_string(), Regressor::additive());
 
