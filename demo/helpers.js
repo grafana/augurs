ffa0424e837717bce4b82c67035b5167f3e8104a
Ben Sully <ben.sully88@gmail.com>
2024-11-18 07:57:05 +0000

    feat: make demo more responsive

--- demo/helpers.js
+++ demo/helpers.js
@@ -1,6 +1,9 @@
 export function getSize() {
+  const margin = window.innerWidth <= 768 ? 30 : 100;
+  const denominator =
+    window.innerWidth <= 480 ? 2.5 : window.innerWidth <= 768 ? 3 : 4;
   return {
-    width: Math.min(window.innerWidth - 100, 1100),
-    height: window.innerHeight / 4,
+    width: Math.min(window.innerWidth - margin, 1100),
+    height: window.innerHeight / denominator,
   };
 }
