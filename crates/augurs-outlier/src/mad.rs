2805404700c91d655ecd5713476a0aeb6108bb2e
Xiangzhuang Shen <datahonor@gmail.com>
2025-02-26 21:34:05 +0800

    remove unwrap

--- crates/augurs-outlier/src/mad.rs
+++ crates/augurs-outlier/src/mad.rs
@@ -95,9 +95,10 @@ impl MADDetector {
     }
 
     /// Set sensitivity for the MAD detector.
-    pub fn set_sensitivity(&mut self, sensitivity: f64) {
+    pub fn set_sensitivity(&mut self, sensitivity: f64) -> Result<(), Error> {
         self.threshold_or_sensitivity =
-            ThresholdOrSensitivity::Sensitivity(sensitivity.try_into().unwrap());
+            ThresholdOrSensitivity::Sensitivity(sensitivity.try_into()?);
+        Ok(())
     }
 
     /// Set the precalculated medians.
