72ce4bfbb3249a2a5673e8f5199dd2f1511522b2
Ben Sully <ben.sully88@gmail.com>
2025-10-08 07:37:54 +0100

    Add comment explaining how to build Wasm before go:generate directives

--- go/changepoint/doc.go
+++ go/changepoint/doc.go
@@ -1,4 +1,5 @@
 // Package changepoint provides changepoint detection algorithms.
 package changepoint
 
+// Run `just go/build-wasm` first to produce the Wasm artifact referenced below.
 //go:generate gravity --world changepoint --wit-file ../../components/rust/changepoint/wit/changepoint.wit --output changepoint.go ../../target/wasm32-unknown-unknown/release/augurs_changepoint_component.wasm
